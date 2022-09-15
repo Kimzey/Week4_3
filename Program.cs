3ebdeaf53911acf29d48c98ba6226500e42e149e
Kimzey <pan.nitiyothin@mail.kmutt.ac.th>
2022-09-15 23:00:58 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program {
+     static void Main (){
+        Console.Write("input number : ");
+        int Number;
+        Number = int.Parse(Console.ReadLine());
+            if (Number >=1){
+                Console.WriteLine("yellow");
+                Number = Number-1;
+            }
+            if (Number >=2){
+                Console.WriteLine("orange");
+                Number = Number-2;
+            }
+            if (Number >= 4){
+                Console.WriteLine("red");
+                Number = Number-4;
+            }
+     }
+}
